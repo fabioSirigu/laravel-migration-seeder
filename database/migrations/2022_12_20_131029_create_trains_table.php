a474a5e496fc216f9896a17499e199cb6223ad8b
naz1ne <fabio.sirigu.1995@gmail.com>
2022-12-20 18:18:13 +0100

    add id and multiple seed option

--- database/migrations/2022_12_20_131029_create_trains_table.php
+++ database/migrations/2022_12_20_131029_create_trains_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('trains', function (Blueprint $table) {
+            $table->id();
             $table->string('company', 50);
             $table->string('departure_station', 80);
             $table->string('arrival_station', 80);
